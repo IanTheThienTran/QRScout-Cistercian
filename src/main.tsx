5bcf5bef5c2968def1d866f0d0e52ef623573cf6
IanTheThienTran <iantran459@gmail.com>
2026-01-23 17:06:59 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './app.tsx';
 import './index.css';
+import { ThemeProvider } from './components/ThemeProvider'; // adjust the path if needed
 
 const root = createRoot(document.getElementById('app')!);
 
-root.render(<App />);
+root.render(
+  <ThemeProvider defaultTheme="dark">
+    <App />
+  </ThemeProvider>
+);
