4bb0f5eb9a2cd5730358854952e763fd7e6e83ad
IanTheThienTran <iantran459@gmail.com>
2026-02-10 23:05:30 -0600

    Add esction theme color

--- src/components/core/Section.tsx
+++ src/components/core/Section.tsx
@@ -7,8 +7,8 @@ type SectionProps = {
 
 export function Section(props: SectionProps) {
   return (
-    <Card className="text-card-foreground">
-      <CardHeader className="pb-2">
+    <Card className="bg-[hsl(var(--section))] text-[hsl(var(--section-foreground))]">
+      <CardHeader className="pb-2 bg-[hsl(var(--section))] text-[hsl(var(--section-foreground))]">
         {props.title && (
           <CardTitle className="font-rhr-ns text-card-foreground">
             {props.title}
